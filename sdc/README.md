441f4f859a42e194dbeb2471ec8ff8c22d4abf2f
Jeff Smith <jasmith@contractor.usgs.gov>
2019-04-23 15:21:09 -0400

    First commit - Basic working reactive search

---
+++ sdc/README.md
@@ -0,0 +1,38 @@
+# Science Data Catalog 2 - ElasticSearch/React Front End Application
+
+This is the repository for the Science Data Catalog version 2.  It uses the JavaScript React framework which connects to a hosted ElasticSearch Application at https://bonsai.io/   The project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Basic setup:
+After checkking out the repo for the first time, you will need to install your node dependencies locally before you can run sdc.  The node_modules directory contains thousands of files and thus it is included in .gitignore.
+
+1.  Check out the repo
+2.  In the project directory, run ```npm install``` - this will install the required node modules.
+
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
---
+++ sdc/src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import {
+	ReactiveBase,
+	MultiList,
+	DataSearch,
+	ReactiveList,
+	SelectedFilters
+} from '@appbaseio/reactivesearch';
+import './App.css';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			visible: false,
+		};
+	}
+
+	toggleState = () => {
+		const { visible } = this.state;
+		this.setState({
+			visible: !visible,
+		});
+	};
+
+	render() {
+		return (
+			<ReactiveBase
+				app="sdc_items"
+				credentials="ixlwb0m0pz:ammkd2jkkk"
+				url="https://sdc-testing-3352148807.us-east-1.bonsaisearch.net"
+			>
+				<div className="navbar">
+					<div className="logo">
+						SDC ReactiveSearch Concept App
+					</div>
+				</div>
+				
+				<div className="display">
+					
+					<div className={`leftSidebar ${this.state.visible ? 'active' : ''}`}>				
+						<div className = "filterHeader">Filter By:</div>
+						<MultiList
+							componentId="Keyword"
+							dataField="metadata.resourceInfo.keyword.keyword.keyword.keyword"
+							title="Keywords"
+						/>	
+						<MultiList
+							componentId="Author"
+							dataField="contact.name.keyword"
+							title="Authors"
+						/>	
+					</div>
+					
+					<div className="mainBar">
+						<SelectedFilters
+    						showClearAll={true}
+    						clearAllLabel="Clear filters"
+						/>
+						<DataSearch
+							componentId="Text Search"
+							dataField={["metadata.resourceInfo.citation.title", "metadata.resourceInfo.abstract"]}
+							autosuggest={true}
+							showFilter={true}
+						/>
+						<ReactiveList
+							componentId="SearchResult"
+							dataField="metadata.resourceInfo.citation.title.keyword"
+							pagination={true}
+							paginationAt="bottom"
+							react={{
+								"and": ["Keyword","Text Search","Author","KeywordCloud"]
+							}}
+							loader="Loading Results.."
+							renderItem={(res) => 
+							
+							<div className="result-data">
+							<div className = "result-item">
+							<div className = "result-title">{res.metadata.resourceInfo.citation.title}</div><br/>
+							<div className = "result-abstract">{res.metadata.resourceInfo.abstract}</div>
+
+							</div>
+							</div>
+							}
+							renderResultStats={
+								function(stats){
+									return (
+										`Showing ${stats.displayedResults} of total ${stats.numberOfResults} in ${stats.time} ms`
+									)
+								}
+							}
+						/>
+						 {/* <TagCloud
+							componentId="KeywordCloud"
+							dataField="metadata.resourceInfo.keyword.keyword.keyword.keyword"
+						/> */}
+					</div>
+				</div>
+			</ReactiveBase>
+		);
+	}
+}
+
+export default App;
