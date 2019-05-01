7ac5d43e1b6067e994817bef24cacb01208c8430
Jeff Smith <jasmith@contractor.usgs.gov>
2019-05-01 16:01:26 -0400

    Some styling changes and switched to boostrap-cards for display.

--- sdc/src/App.js
+++ sdc/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {ReactiveBase, MultiList, DataSearch, ReactiveList, SelectedFilters} from '@appbaseio/reactivesearch';
 import Usgsheader from './bundles/usgs/usgsheader.js';
 import Usgsfooter from './bundles/usgs/usgsfooter.js';
+import {Card} from 'react-bootstrap';
 import './App.css';
 import './bundles/usgs/common.css';
 import './bundles/usgs/custom.css';
@@ -23,7 +24,7 @@ class App extends Component {
 
     render() {
         return (
-            
+
             <ReactiveBase
                 app="sdc_items"
                 credentials="ixlwb0m0pz:ammkd2jkkk"
@@ -31,7 +32,8 @@ class App extends Component {
                 <Usgsheader/>
                 <div className="navbar">
                     <div className="logo">
-                        USGS Science Data Catalog - Proof of Concept App using React Framework and Elasticsearch
+                        USGS Science Data Catalog - Proof of Concept App using React Framework and
+                        Elasticsearch
                     </div>
                 </div>
                 <div className="display">
@@ -46,7 +48,7 @@ class App extends Component {
                             title="Keywords"
                             URLParams={true}
                             react={{
-                            "and": ["Keyword", "Author", "Text Search"]
+                            "and": ["Keyword", "Author"]
                         }}/>
                         <MultiList
                             componentId="Author"
@@ -54,7 +56,7 @@ class App extends Component {
                             title="Authors"
                             URLParams={true}
                             react={{
-                            "and": ["Keyword", "Author", "Text Search"]
+                            "and": ["Keyword", "Author"]
                         }}/>
                     </div>
 
@@ -70,16 +72,20 @@ class App extends Component {
                             componentId="SearchResult"
                             dataField="metadata.resourceInfo.citation.title.keyword"
                             pagination={true}
-                            paginationAt="bottom"
+                            paginationAt="both"
                             react={{
                             "and": ["Keyword", "Text Search", "Author"]
                         }}
                             loader="Loading Results.."
                             renderItem={(res) => <div className="result-data">
-                            <div className="result-item">
-                                <div className="result-title">{res.metadata.resourceInfo.citation.title}</div><br/>
-                                <div className="result-abstract">{res.metadata.resourceInfo.abstract}</div>
-                            </div>
+                            <Card border="dark">
+                                <Card.Header className="result-title">{res.metadata.resourceInfo.citation.title}</Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        {res.metadata.resourceInfo.abstract}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         </div>}
                             renderResultStats={function (stats) {
                             return (`Showing ${stats.displayedResults} of total ${stats.numberOfResults} in ${stats.time} ms`)
@@ -91,7 +97,7 @@ class App extends Component {
                 </div>
                 <Usgsfooter/>
             </ReactiveBase>
-            
+
         );
     }
 }
