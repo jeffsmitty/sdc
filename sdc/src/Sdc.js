eb642238f1aa5e6d25df7e80da01f0f62a44da0e
Jeff Smith <jasmith@contractor.usgs.gov>
2019-05-02 16:25:55 -0400

    Added react routing and set up a home page and stub for item landing page

--- sdc/src/Sdc.js
+++ sdc/src/Sdc.js
@@ -1,4 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./Sdc.css";
+import "./bundles/usgs/common.css";
+import "./bundles/usgs/custom.css";
+import Usgsheader from "./bundles/usgs/usgsheader.js";
+import Usgsfooter from "./bundles/usgs/usgsfooter.js";
 import {
   ReactiveBase,
   MultiList,
@@ -6,44 +12,59 @@ import {
   ReactiveList,
   SelectedFilters
 } from "@appbaseio/reactivesearch";
-import Usgsheader from "./bundles/usgs/usgsheader.js";
-import Usgsfooter from "./bundles/usgs/usgsfooter.js";
 import { Card } from "react-bootstrap";
-import "./App.css";
-import "./bundles/usgs/common.css";
-import "./bundles/usgs/custom.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false
-    };
-  }
+function SdcApp() {
+  return (
+    <div>
+      <Usgsheader />
+      <div className="navbar">
+        <div className="logo">
+          USGS Science Data Catalog - Proof of Concept App using React Framework
+          and Elasticsearch
+        </div>
+      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">SDC Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/items">Item Landing Page</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Sdchome} />
+          <Route path="/search" component={Search} />
+          <Route path="/items" component={Items} />
+        </div>
+      </Router>
+      <Usgsfooter />
+    </div>
+  );
+}
 
-  toggleState = () => {
-    const { visible } = this.state;
-    this.setState({
-      visible: !visible
-    });
-  };
+function Sdchome() {
+  return (
+    <div>
+      <h2>Science Data Catalog Home</h2>
+    </div>
+  );
+}
 
-  render() {
-    return (
+function Search() {
+  return (
+    <div>
       <ReactiveBase
         app="sdc_items"
         credentials="ixlwb0m0pz:ammkd2jkkk"
         url="https://sdc-testing-3352148807.us-east-1.bonsaisearch.net"
       >
-        <Usgsheader />
-        <div className="navbar">
-          <div className="logo">
-            USGS Science Data Catalog - Proof of Concept App using React
-            Framework and Elasticsearch
-          </div>
-        </div>
         <div className="display">
-          <div className={`leftSidebar ${this.state.visible ? "active" : ""}`}>
+          <div className="leftSidebar">
             <div className="filterHeader">Filter By:</div>
             <MultiList
               componentId="Keyword"
@@ -108,17 +129,44 @@ class App extends Component {
                   stats.numberOfResults
                 } in ${stats.time} ms`;
               }}
-            />{" "}
-            {/* <TagCloud
-							componentId="KeywordCloud"
-							dataField="metadata.resourceInfo.keyword.keyword.keyword.keyword"
-						/> */}
+            />
           </div>
         </div>
-        <Usgsfooter />
       </ReactiveBase>
-    );
-  }
+    </div>
+  );
 }
 
-export default App;
+function Items({ match }) {
+  return (
+    <div>
+      <h2>Item Landing Page</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/1`}>Item 1</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/2`}>Item 2</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:itemId`} component={Item} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select an item</h3>}
+      />
+    </div>
+  );
+}
+
+function Item({ match }) {
+  console.info(match.params.itemId);
+  return (
+    <div>
+      <h3>Item {match.params.itemId}</h3>
+    </div>
+  );
+}
+
+export default SdcApp;
--- sdc/src/Sdc.test.js
+++ sdc/src/Sdc.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Sdc from './Sdc';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Sdc />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
