d9f8758815c0207d4803468510d9a8bc456cbee3
Jeff Smith <jasmith@contractor.usgs.gov>
2019-05-03 12:32:32 -0400

    Css consolidation.  Added bootstrap style breadcrumbs

--- sdc/src/Sdc.js
+++ sdc/src/Sdc.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./Sdc.css";
 import "./bundles/usgs/common.css";
-import "./bundles/usgs/custom.css";
 import Usgsheader from "./bundles/usgs/usgsheader.js";
 import Usgsfooter from "./bundles/usgs/usgsfooter.js";
 import {
@@ -12,12 +11,13 @@ import {
   ReactiveList,
   SelectedFilters
 } from "@appbaseio/reactivesearch";
-import { Card } from "react-bootstrap";
+import { Card, Breadcrumb } from "react-bootstrap";
 
 function SdcApp() {
   return (
     <div>
       <Usgsheader />
+      <div className="subheader" />
       <div className="navbar">
         <div className="logo">
           USGS Science Data Catalog - Proof of Concept App using React Framework
@@ -26,17 +26,6 @@ function SdcApp() {
       </div>
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">SDC Home</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-            <li>
-              <Link to="/items">Item Landing Page</Link>
-            </li>
-          </ul>
           <Route exact path="/" component={Sdchome} />
           <Route path="/search" component={Search} />
           <Route path="/items" component={Items} />
@@ -50,7 +39,14 @@ function SdcApp() {
 function Sdchome() {
   return (
     <div>
+    <Breadcrumb>
+    <Breadcrumb.Item active><Link to="https://www.usgs.gov/core-science-systems/science-analytics-and-synthesis">Science, Analytics, and Synthesis</Link></Breadcrumb.Item>
+    <Breadcrumb.Item active>Science Data Catalog Home</Breadcrumb.Item>
+  </Breadcrumb>
+    <div>
       <h2>Science Data Catalog Home</h2>
+      <Link to="/search">Search the catalog</Link>
+    </div>
     </div>
   );
 }
@@ -58,81 +54,88 @@ function Sdchome() {
 function Search() {
   return (
     <div>
-      <ReactiveBase
-        app="sdc_items"
-        credentials="ixlwb0m0pz:ammkd2jkkk"
-        url="https://sdc-testing-3352148807.us-east-1.bonsaisearch.net"
-      >
-        <div className="display">
-          <div className="leftSidebar">
-            <div className="filterHeader">Filter By:</div>
-            <MultiList
-              componentId="Keyword"
-              dataField="metadata.resourceInfo.keyword.keyword.keyword.keyword"
-              title="Keywords"
-              URLParams={true}
-              react={{
-                and: ["Keyword", "Author"]
-              }}
-            />
-            <MultiList
-              componentId="Author"
-              dataField="contact.name.keyword"
-              title="Authors"
-              URLParams={true}
-              react={{
-                and: ["Keyword", "Author"]
-              }}
-            />
-          </div>
+      <Breadcrumb>
+      <Breadcrumb.Item active><Link to="https://www.usgs.gov/core-science-systems/science-analytics-and-synthesis">Science, Analytics, and Synthesis</Link></Breadcrumb.Item>
+        <Breadcrumb.Item><Link to="/">Science Data Catalog Home</Link></Breadcrumb.Item>
+        <Breadcrumb.Item active>Search</Breadcrumb.Item>
+      </Breadcrumb>
+      <div>
+        <ReactiveBase
+          app="sdc_items"
+          credentials="ixlwb0m0pz:ammkd2jkkk"
+          url="https://sdc-testing-3352148807.us-east-1.bonsaisearch.net"
+        >
+          <div className="display">
+            <div className="leftSidebar">
+              <div className="filterHeader">Filter By:</div>
+              <MultiList
+                componentId="Keyword"
+                dataField="metadata.resourceInfo.keyword.keyword.keyword.keyword"
+                title="Keywords"
+                URLParams={true}
+                react={{
+                  and: ["Keyword", "Author"]
+                }}
+              />
+              <MultiList
+                componentId="Author"
+                dataField="contact.name.keyword"
+                title="Authors"
+                URLParams={true}
+                react={{
+                  and: ["Keyword", "Author"]
+                }}
+              />
+            </div>
 
-          <div className="mainBar">
-            <SelectedFilters
-              showClearAll={true}
-              clearAllLabel="Clear filters"
-            />
-            <DataSearch
-              componentId="Text Search"
-              dataField={[
-                "metadata.resourceInfo.citation.title",
-                "metadata.resourceInfo.abstract"
-              ]}
-              autosuggest={true}
-              showFilter={true}
-              URLParams={true}
-            />
-            <ReactiveList
-              componentId="SearchResult"
-              dataField="metadata.resourceInfo.citation.title.keyword"
-              pagination={true}
-              paginationAt="both"
-              react={{
-                and: ["Keyword", "Text Search", "Author"]
-              }}
-              loader="Loading Results.."
-              renderItem={res => (
-                <div className="result-data">
-                  <Card border="dark">
-                    <Card.Header className="result-title">
-                      {res.metadata.resourceInfo.citation.title}
-                    </Card.Header>
-                    <Card.Body>
-                      <Card.Text>
-                        {res.metadata.resourceInfo.abstract}
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                </div>
-              )}
-              renderResultStats={function(stats) {
-                return `Showing ${stats.displayedResults} of total ${
-                  stats.numberOfResults
-                } in ${stats.time} ms`;
-              }}
-            />
+            <div className="mainBar">
+              <SelectedFilters
+                showClearAll={true}
+                clearAllLabel="Clear filters"
+              />
+              <DataSearch
+                componentId="Text Search"
+                dataField={[
+                  "metadata.resourceInfo.citation.title",
+                  "metadata.resourceInfo.abstract"
+                ]}
+                autosuggest={true}
+                showFilter={true}
+                URLParams={true}
+              />
+              <ReactiveList
+                componentId="SearchResult"
+                dataField="metadata.resourceInfo.citation.title.keyword"
+                pagination={true}
+                paginationAt="both"
+                react={{
+                  and: ["Keyword", "Text Search", "Author"]
+                }}
+                loader="Loading Results.."
+                renderItem={res => (
+                  <div className="result-data">
+                    <Card border="dark">
+                      <Card.Header className="result-title">
+                        {res.metadata.resourceInfo.citation.title}
+                      </Card.Header>
+                      <Card.Body>
+                        <Card.Text>
+                          {res.metadata.resourceInfo.abstract}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                )}
+                renderResultStats={function(stats) {
+                  return `Showing ${stats.displayedResults} of total ${
+                    stats.numberOfResults
+                  } in ${stats.time} ms`;
+                }}
+              />
+            </div>
           </div>
-        </div>
-      </ReactiveBase>
+        </ReactiveBase>
+      </div>
     </div>
   );
 }
--- sdc/src/bundles/usgs/usgsfooter.js
+++ sdc/src/bundles/usgs/usgsfooter.js
@@ -3,137 +3,137 @@ import React from "react";
 class Usgsheader extends React.Component {
   render() {
     return (
-      <footer class="footer">
-        <div class="tmp-container">
-          <div class="footer-sdc">
-            <ul class="menu nav">
-              <li class="first leaf menu-links menu-level-1">
+      <footer className="footer">
+        <div className="tmp-container">
+          <div className="footer-sdc">
+            <ul className="menu nav">
+              <li className="first leaf menu-links menu-level-1">
                 <a href="https://www.doi.gov/privacy">DOI Privacy Policy</a>
               </li>
-              <li class="leaf menu-links menu-level-1">
+              <li className="leaf menu-links menu-level-1">
                 <a href="https://www.usgs.gov/laws/policies_notices.html">
                   Legal
                 </a>
               </li>
-              <li class="leaf menu-links menu-level-1">
+              <li className="leaf menu-links menu-level-1">
                 <a href="https://www2.usgs.gov/laws/accessibility.html">
                   Accessibility
                 </a>
               </li>
-              <li class="leaf menu-links menu-level-1">
+              <li className="leaf menu-links menu-level-1">
                 <a href="https://www.usgs.gov/sitemap.html">Site Map</a>
               </li>
-              <li class="last leaf menu-links menu-level-1">
+              <li className="last leaf menu-links menu-level-1">
                 <a href="https://answers.usgs.gov/">Contact USGS</a>
               </li>
             </ul>
           </div>
           <hr />
-          <div class="footer-sdc">
-            <ul class="menu nav">
-              <li class="first leaf menu-links menu-level-1">
+          <div className="footer-sdc">
+            <ul className="menu nav">
+              <li className="first leaf menu-links menu-level-1">
                 <a href="https://www.doi.gov/">
                   U.S. Department of the Interior
                 </a>
               </li>
-              <li class="leaf menu-links menu-level-1">
+              <li className="leaf menu-links menu-level-1">
                 <a href="https://www.doioig.gov/">DOI Inspector General</a>
               </li>
-              <li class="leaf menu-links menu-level-1">
+              <li className="leaf menu-links menu-level-1">
                 <a href="https://www.whitehouse.gov/">White House</a>
               </li>
-              <li class="leaf menu-links menu-level-1">
+              <li className="leaf menu-links menu-level-1">
                 <a href="https://www.whitehouse.gov/omb/management/egov/">
                   E-gov
                 </a>
               </li>
-              <li class="leaf menu-links menu-level-1">
+              <li className="leaf menu-links menu-level-1">
                 <a href="https://www.doi.gov/pmb/eeo/no-fear-act">
                   No Fear Act
                 </a>
               </li>
-              <li class="last leaf menu-links menu-level-1">
+              <li className="last leaf menu-links menu-level-1">
                 <a href="https://www2.usgs.gov/foia">FOIA</a>
               </li>
             </ul>
           </div>
-          <div class="footer-social-links">
-            <ul class="social">
-              <li class="follow">Follow</li>
-              <li class="twitter">
+          <div className="footer-social-links">
+            <ul className="social">
+              <li className="follow">Follow</li>
+              <li className="twitter">
                 <a
                   href="https://twitter.com/usgs"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fa fa-twitter-square">
-                    <span class="only">Twitter</span>
+                  <i className="fa fa-twitter-square">
+                    <span className="only">Twitter</span>
                   </i>
                 </a>
               </li>
-              <li class="facebook">
+              <li className="facebook">
                 <a
                   href="https://facebook.com/usgeologicalsurvey"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fa fa-facebook-square">
-                    <span class="only">Facebook</span>
+                  <i className="fa fa-facebook-square">
+                    <span className="only">Facebook</span>
                   </i>
                 </a>
               </li>
-              <li class="googleplus">
+              <li className="googleplus">
                 <a
                   href="https://plus.google.com/112624925658443863798/posts"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fa fa-google-plus-square">
-                    <span class="only">Google+</span>
+                  <i className="fa fa-google-plus-square">
+                    <span className="only">Google+</span>
                   </i>
                 </a>
               </li>
-              <li class="github">
+              <li className="github">
                 <a
                   href="https://github.com/usgs"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fa fa-github">
-                    <span class="only">GitHub</span>
+                  <i className="fa fa-github">
+                    <span className="only">GitHub</span>
                   </i>
                 </a>
               </li>
-              <li class="flickr">
+              <li className="flickr">
                 <a
                   href="https://flickr.com/usgeologicalsurvey"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fa fa-flickr">
-                    <span class="only">Flickr</span>
+                  <i className="fa fa-flickr">
+                    <span className="only">Flickr</span>
                   </i>
                 </a>
               </li>
-              <li class="youtube">
+              <li className="youtube">
                 <a
                   href="http://youtube.com/usgs"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fa fa-youtube-play">
-                    <span class="only">YouTube</span>
+                  <i className="fa fa-youtube-play">
+                    <span className="only">YouTube</span>
                   </i>
                 </a>
               </li>
-              <li class="instagram">
+              <li className="instagram">
                 <a
                   href="https://instagram.com/usgs"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fa fa-instagram">
-                    <span class="only">Instagram</span>
+                  <i className="fa fa-instagram">
+                    <span className="only">Instagram</span>
                   </i>
                 </a>
               </li>
